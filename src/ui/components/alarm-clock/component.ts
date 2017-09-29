d3ea0f82c1409b7b0982e9581753691a5ead1e09
Jacob Mishkin <user@MacBook-Pro.local>
2017-09-29 12:22:50 -0500

    looped through day of the week to display

--- src/ui/components/alarm-clock/component.ts
+++ src/ui/components/alarm-clock/component.ts
@@ -2,6 +2,7 @@ import Component, { tracked } from '@glimmer/component';
 
 export default class AlarmClock extends Component {
 	@tracked time;
+	@tracked weekdays;
 
 	constructor(options) {
 		super(options);
@@ -16,8 +17,22 @@ export default class AlarmClock extends Component {
 			if(i === 1) {key = 'minutes';}
 			if(i === 2) {key = 'seconds';}
 			timeObject[key] = [time[0], time[1]];
-		});
+		});	
 		this.time = timeObject;
 		setTimeout(() => this.loadTime(), 1000);
+		
+		let activeDay = new Date().getDay();
+		let weekdays = ['Mon', 'Tues', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+		this.weekdays = weekdays.map((day, i ) => {
+			let cssClass = '';
+			i = i + 1; // 1 based index to follow getday();
+			if( i === activeDay ) { 
+				cssClass = 'active';
+			}
+			return { day, cssClass };
+		});
+
+
+
 	}
 }
