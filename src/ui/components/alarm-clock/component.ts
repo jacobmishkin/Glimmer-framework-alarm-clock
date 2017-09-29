edf19f438c309e562b9510edd0a716321608c25e
Jacob Mishkin <user@MacBook-Pro.local>
2017-09-29 11:05:39 -0500

    created function to get current time and looped through HTML

--- src/ui/components/alarm-clock/component.ts
+++ src/ui/components/alarm-clock/component.ts
@@ -1,5 +1,23 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
 
 export default class AlarmClock extends Component {
+	@tracked time;
 
+	constructor(options) {
+		super(options);
+		this.loadTime();
+	}
+
+	loadTime() {
+		let time = new Date().toLocaleTimeString();
+		let timeObject = {};
+		time.split(':').forEach((time, i) => {
+			let key = 'hours';
+			if(i === 1) {key = 'minutes';}
+			if(i === 2) {key = 'seconds';}
+			timeObject[key] = [time[0], time[1]];
+		});
+		this.time = timeObject;
+
+	}
 }
